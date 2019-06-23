dc86feea70ccee6ab62e2668e40ceca666beae3c
wglm2014 <glinaresmiranda@yahoo.com>
2019-06-22 23:32:33 -0400

    all functional

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,34 +2,27 @@
 const friends = [{
     fullName: "wilson linares",
     foto: "https://www.dreamgrow.com/wp-content/uploads/2017/07/free-image-sites-cover-frog-720x512.jpg",
-    scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 }, {
     fullName: "Ana Maria",
     foto: "https://www.dreamgrow.com/wp-content/uploads/2017/07/free-image-sites-cover-frog-720x512.jpg",
-    scores: [
-        5,
-        4,
-        4,
-        6,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
+    scores: [5, 4, 4, 6, 5, 1, 2, 5, 4, 1]
+}, {
+    fullName: "Ahmed",
+    foto: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+}, {
+    fullName: "Jacob Deming",
+    foto: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+}, {
+    fullName: "Jeremiah Scanlon",
+    foto: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+}, {
+    fullName: "Louis T. Delia",
+    foto: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
 }];
 
 module.exports = friends;
-
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,23 +8,27 @@ router.get("/api/friends", function (req, res) {
 });
 
 router.post("/api/friends", function (req, res) {
-    constnewFriend = req.body;
+    const newFriend = req.body;
     const matchingFriend = searchMatches(newFriend);
+    console.log(`matching friend ${matchingFriend}`);
     friends.push = {
         fullName: newFriend.fullName, foto: newFriend.foto,
         scores: newFriend.scores
     };
-    console.log(req.body);
+    console.log(friends);
+    console.log(matchingFriend);
     res.json(matchingFriend);
 
 });
 
 function searchMatches(newFriend) {
+    console.log(newFriend)
     let oldPoints = 0;
     let newPoints = 0;
     let highPoints = 0;
-    matchingFriend = [];
+    matchingFriend = {};
     friends.forEach(element => {
+        console.log(element.scores);
         newPoints = accumulatePoints(element.scores, newFriend.scores);
         if (oldPoints < newPoints) {
             highPoints = newPoints;
@@ -32,14 +36,21 @@ function searchMatches(newFriend) {
         }
         oldPoints = newPoints;
     });
+    if (highPoints === 0) {
+        matchingFriend.fullName = "no match found, sorry";
+        matchingFriend.foto = "none";
+    }
+    console.log(`highest points ${highPoints} ${matchingFriend.fullName}`);
     return matchingFriend;
 }
 
 function accumulatePoints(scoresMatchingFriend, scoresNewFriend) {
     let points = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (scoresMatchingFriend[i] === scoresNewFriend[i]) {
-            points++
+    for (let i = 0; i < scoresNewFriend.length; i++) {
+        console.log(typeof (scoresMatchingFriend[i]) + " and " + typeof (scoresNewFriend[i]));
+        if (+scoresMatchingFriend[i] === +scoresNewFriend[i]) {
+            points++;
+            console.log(points);
         }
     }
     return points;
